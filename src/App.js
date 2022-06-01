92ed7a9a0ad8470ee98733284751f7018a03905e
Dibyajyoti Choudhury <66021521+Dibyajyoti470@users.noreply.github.com>
2022-06-02 00:33:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Signup.js
@@ -0,0 +1,59 @@
+import { React } from "react";
+import Input from "../components/Input";
+import PasswordInput from "../components/PasswordInput";
+
+const Signup = () => {
+  return (
+    <main className="signup-container">
+      <h1 className="heading-1 text-center">Create an account</h1>
+      <span className="text-gray">Fill in the following details</span>
+      <form className="form signup-form" action="#">
+        <Input
+          classname="input-field"
+          label="Name"
+          name="name"
+          type="text"
+          isRequired={true}
+        />
+        <Input
+          classname="input-field"
+          label="Organization"
+          name="org"
+          type="text"
+          isRequired={true}
+        />
+        <Input
+          classname="input-field"
+          label="Email"
+          name="email"
+          type="email"
+          isRequired={true}
+        />
+        <PasswordInput classname="input-field" label="Password" name="pass" />
+        <span className="text-gray paragraph">
+          By creating this account, I am agreeing to the{" "}
+          <a className="link bold" href="#">
+            Privacy Policy
+          </a>{" "}
+          and{" "}
+          <a className="link bold" href="#">
+            Terms of Use
+          </a>
+          .
+        </span>
+        <button className="btn primary-btn" type="submit">
+          Create account
+        </button>
+      </form>
+      <span className="text-gray">
+        Already have an account?{" "}
+        <a className="link bold" href="#">
+          Login
+        </a>{" "}
+        instead
+      </span>
+    </main>
+  );
+};
+
+export default Signup;
